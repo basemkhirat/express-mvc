ffa68e206a785703e5b97c84f25b8c6edc8616e5
basemkhirat <basemkhirat@gmail.com>
2017-10-31 13:58:16 +0400

    add trust proxy config

--- app.js
+++ app.js
@@ -28,6 +28,6 @@ app.set("env", _config("app.env"));
 app.set("views", _config("app.views"));
 app.set("view engine", _config("app.view_engine"));
 app.set("x-powered-by", _config("app.x_powered_by"));
-app.set('trust proxy', 1);
+app.set('trust proxy', _config("app.trust_proxy"));
 
 module.exports = app;
--- config/app.js
+++ config/app.js
@@ -20,6 +20,15 @@ module.exports = {
     port: process.env.PORT || 3000,
 
     /**
+     * Enabling trust proxy will have the following impact:
+     * The value of req.hostname is derived from the value set in the X-Forwarded-Host header, which can be set by the client or by the proxy.
+     * X-Forwarded-Proto can be set by the reverse proxy to tell the app whether it is https or http or even an invalid name. This value is reflected by req.protocol.
+     * The req.ip and req.ips values are populated with the list of addresses from X-Forwarded-For.
+     */
+
+    trust_proxy: true,
+
+    /**
      * The x-powered-by header key
      */
 
